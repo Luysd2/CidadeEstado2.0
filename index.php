b2e0c71c6c3d77354b8679ea60d2de93ed09c2ae
Luis Sousa <luysd2@gmail.com>
2021-12-11 20:25:24 -0300

    Pagina inicial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Principal</title>
+</head>
+<body>
+    <h3>tela inicial</h3>
+    <p>Aqui neste tela vc pode cadastrar o estado e cidade</p>
+    <a href="cadastro.php">Cadastrar</a>
+    <br><br>
+    <a href="pesquisa.php">Pesquisar</a>
+</body>
+</html>
